e2927b823e9ebc312ddbc2adb324338844d3a35b
webnovelty <webnoveltystudio@gmail.com>
2022-08-17 20:31:07 +0200

    final

--- src/index.js
+++ src/index.js
@@ -1,14 +1,56 @@
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries.js';
 
 const DEBOUNCE_DELAY = 300;
 const inputEl = document.querySelector('#search-box');
+const listEl = document.querySelector('.country-list');
+
+
 
 
 inputEl.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 function onSearch() {
+	listEl.innerHTML = '';
 	if (inputEl.value !== '') {
-		fetchCountries(inputEl.value);
+
+		trimInput = inputEl.value.trim();
+		fetchCountries(trimInput)
+			.then(d => {
+				if (d.length > 10) {
+					Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+				}
+				if (d.length >= 2 && d.length <= 10) {
+
+					listEl.insertAdjacentHTML('beforeend', createListItems(d));
+					function createListItems(d) {
+						return d.map((elementItem) => {
+							return `<li class="flags"><img class="img-flag" src="${elementItem.flags.svg}"/>${elementItem.name.official}</li>`;
+						}).join('');
+
+					}
+
+				}
+				if (d.length === 1) {
+					listEl.insertAdjacentHTML('beforeend', createListItems(d));
+					function createListItems(d) {
+						return d.map((elementItem) => {
+
+							return `<div class="flags"><img class="img-flag" src="${elementItem.flags.svg}"/>${elementItem.name.official}</div>
+							<div class="info"><span class="title-info">Capita:</span> ${elementItem.capital}</div>
+							<div class="info"><span class="title-info">Population:</span> ${elementItem.population}</div>
+							<div class="info"><span class="title-info">Languages:</span> ${Object.values(elementItem.languages)}</div>`
+						}).join('');
+
+					}
+				}
+			}
+			)
+			.catch(error => {
+				Notiflix.Notify.failure(`Oops, there is no country with that name`);
+			})
+			;
+
 	}
 }
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,6 +1,13 @@
 export function fetchCountries(name) {
-	const url = `https://restcountries.com/v3.1/name/${name}`;
+	const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
 	return fetch(url)
-		.then(r => r.json())
-		.then(d => (console.log(d)));
+		.then(r => {
+			if (!r.ok) {
+				throw new Error(r.status);
+			}
+			return r.json()
+		})
+		.then(d => {
+			return d;
+		});
 }
