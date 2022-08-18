1a6709f4d98facf9b80291ad5347915ff56ae847
webnovelty <webnoveltystudio@gmail.com>
2022-08-19 00:59:04 +0200

    minus-then

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,13 +1,11 @@
 export function fetchCountries(name) {
 	const url = `https://restcountries.com/v3.1/name/${name}?fields=name,capital,population,flags,languages`;
 	return fetch(url)
-		.then(r => {
-			if (!r.ok) {
-				throw new Error(r.status);
-			}
-			return r.json()
-		})
 		.then(d => {
-			return d;
-		});
+			if (!d.ok) {
+				throw new Error(d.status);
+			}
+			return d.json()
+		})
+		;
 }
